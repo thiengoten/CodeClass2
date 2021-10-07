7e035cc3609fc93dc6f500524b9e6e5c8e548738
thiengoten <thiengoten6@gmail.com>
2021-10-07 20:29:27 +0700

    setup sequelize and database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const database = require("./database/models/index");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,5 +38,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+database.testConnection();
 
 module.exports = app;
